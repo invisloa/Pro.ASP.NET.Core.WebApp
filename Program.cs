d43aceb8ae52f3a3d5f1e6d07be84f3098d335d2
invisloa <91217120+invisloa@users.noreply.github.com>
2023-04-03 00:10:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(opts => {
+	opts.UseSqlServer(builder.Configuration[
+	"ConnectionStrings:ProductConnection"]);
+	opts.EnableSensitiveDataLogging(true);
+});
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+var context = app.Services.CreateScope().ServiceProvider
+.GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
+app.Run();
