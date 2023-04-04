e9bd0d00638eef6dd4a3e58547f21ff435bf59e9
invisloa <91217120+invisloa@users.noreply.github.com>
2023-04-04 22:30:09 +0200

    views and controllers

--- Controllers/SuppliersController.cs
+++ Controllers/SuppliersController.cs
@@ -42,4 +42,5 @@ namespace WebApp.Controllers
 			return s;
 		}
 
-	}}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Models;
-
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<DataContext>(opts => {
 	opts.UseSqlServer(builder.Configuration[
 	"ConnectionStrings:ProductConnection"]);
 	opts.EnableSensitiveDataLogging(true);
 });
-builder.Services.AddControllers()
-.AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 
-builder.Services.Configure<MvcNewtonsoftJsonOptions>(opts => {
-opts.SerializerSettings.NullValueHandling
-= Newtonsoft.Json.NullValueHandling.Ignore;
-});
+builder.Services.AddControllersWithViews();
 
-builder.Services.Configure<MvcOptions>(opts => {
-	opts.RespectBrowserAcceptHeader = true;
-	opts.ReturnHttpNotAcceptable = true;
-});
 
-builder.Services.AddSwaggerGen(c => 
-	{
-		c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApp", Version = "v1" });
-	});
+builder.Services.AddControllers();
 
 var app = builder.Build();
-app.MapControllers();
-app.MapGet("/", () => "Hello World!");
 
-app.UseSwagger();
-app.UseSwaggerUI(options => 
-	{
-		options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp");
-	});
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.MapDefaultControllerRoute();
 
 var context = app.Services.CreateScope().ServiceProvider
 .GetRequiredService<DataContext>();
+
 SeedData.SeedDatabase(context);
+
 app.Run();
