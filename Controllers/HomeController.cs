024a85c4d77936d31394af7708d7476f245a2df7
invisloa <91217120+invisloa@users.noreply.github.com>
2023-04-05 10:17:18 +0200

    partial views content encoding

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace WebApp.Controllers
 		{
 			return View(context.Products);
 		}
+		public IActionResult Html()
+		{
+			return View((object)"This is a <h3><i>string</i></h3>");
+		}
 	}
 }
